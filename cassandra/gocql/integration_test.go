0ec6811e7dc6df4b9510c8fbf1c8f9e32b60e787
Mayur Kale <mayurkale@google.com>
2026-05-19 14:47:46 +0530

    Update integration_test.go

--- cassandra/gocql/integration_test.go
+++ cassandra/gocql/integration_test.go
@@ -84,7 +84,7 @@ func TestMain(m *testing.M) {
 	var err error
 	keyspace, err = generateKeySpaceName()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf("%s", err.Error())
 	}
 
 	switch target {
